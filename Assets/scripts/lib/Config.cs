042d9695100299c4a29910cd89004d93a59c76f4
Oleg Timofeev <oleg.timofeev20@gmail.com>
2015-03-14 18:50:15 +0200

    v 0.0.1

---
+++ Assets/scripts/lib/Config.cs
@@ -0,0 +1,12 @@
+﻿[System.Serializable]
+public class Config {
+
+    public string ServerAddres = "";
+    public string ServerUser = "";
+    public string ServerPass = "";
+
+    public string UserName = "";
+    public string UserPass = "";
+
+    public bool AutoConnect = false;
+}
---
+++ Assets/scripts/lib/FTPConnector.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Net;
+using System.IO;
+
+public class FTPConnector : MonoBehaviour {
+
+    private string Addres;
+    private string User;
+    private string Pass;
+
+    public FTPConnector(string addres, string user, string pass)
+    {
+        this.Addres = addres;
+        this.User = user;
+        this.Pass = pass;
+    }
+
+    public void ListDir()
+    {
+        // Get the object used to communicate with the server.
+        FtpWebRequest request = (FtpWebRequest)WebRequest.Create(Addres);
+        request.Method = WebRequestMethods.Ftp.ListDirectoryDetails;
+
+        // This example assumes the FTP site uses anonymous logon.
+        request.Credentials = new NetworkCredential(User, Pass);
+
+        FtpWebResponse response = (FtpWebResponse)request.GetResponse();
+
+        Stream responseStream = response.GetResponseStream();
+        StreamReader reader = new StreamReader(responseStream);
+        Debug.Log(reader.ReadToEnd());
+            
+        reader.Close();
+        response.Close();
+    }
+}
